3854f2c85f7e79260f48facc153512af38818c07
PH-Palmito <phpalmito@gmail.com>
2025-01-13 10:42:29 -0300

    Initial commit

---
+++ src/componentes/Ordenacao/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+export default function Ordenacao() {
+    return (
+        <section className="container__ordenacao">
+            <button className='Recentes'>Recentes</button>
+            <button className='Relevancia'>Relevância</button>
+        </section>
+    )
+}
