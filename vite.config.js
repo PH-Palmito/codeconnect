3708b91e4600965a7dbb1ec2e9beaf15bd1132b0
PH-Palmito <phpalmito@gmail.com>
2025-01-13 10:49:11 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ svgr(), react()]
+  plugins: [  react()],
+  base: '/codeconnect/'
 })
